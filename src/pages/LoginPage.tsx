3008b2857d77de61efaee7cd06e52f0b82a9456a
Ronald Cabrera <rhcabrerag@outlook.com>
2025-02-25 18:41:15 -0500

    u. Add Admin Account Legend

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,75 +1,76 @@
-import { useState } from 'react';
-import { LuLoader } from 'react-icons/lu';
-import { Link, Navigate } from 'react-router-dom';
-import { useLogin, useUser } from '../hooks';
-import { Loader } from '../components/shared/Loader';
+import { useState } from "react";
+import { LuLoader } from "react-icons/lu";
+import { Link, Navigate } from "react-router-dom";
+import { useLogin, useUser } from "../hooks";
+import { Loader } from "../components/shared/Loader";
 
 export const LoginPage = () => {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	const { mutate, isPending } = useLogin();
-	const { session, isLoading } = useUser();
+  const { mutate, isPending } = useLogin();
+  const { session, isLoading } = useUser();
 
-	const onLogin = (e: React.FormEvent) => {
-		e.preventDefault();
+  const onLogin = (e: React.FormEvent) => {
+    e.preventDefault();
 
-		mutate({ email, password });
-	};
+    mutate({ email, password });
+  };
 
-	if (isLoading) return <Loader />;
+  if (isLoading) return <Loader />;
 
-	if (session) return <Navigate to='/' />;
+  if (session) return <Navigate to="/" />;
 
-	return (
-		<div className='flex flex-col items-center h-full gap-5 mt-12'>
-			<h1 className='text-4xl font-bold capitalize'>
-				Iniciar sesión
-			</h1>
+  return (
+    <div className="flex flex-col items-center h-full gap-5 mt-12">
+      <h1 className="text-4xl font-bold capitalize">Iniciar sesión</h1>
 
-			<p className='text-sm font-medium'>
-				¡Que bueno tenerte de vuelta!
-			</p>
+      <p className="text-sm font-medium">¡Que bueno tenerte de vuelta!</p>
 
-			{isPending ? (
-				<div className='flex justify-center w-full h-full mt-20'>
-					<LuLoader className='animate-spin' size={60} />
-				</div>
-			) : (
-				<>
-					<form
-						className='flex flex-col items-center gap-4 w-full mt-10 sm:w-[400px] lg:w-[500px]'
-						onSubmit={onLogin}
-					>
-						<input
-							type='email'
-							placeholder='Ingresa tu correo electrónico'
-							className='w-full px-5 py-4 text-sm text-black border rounded-full border-slate-200 placeholder:text-black'
-							value={email}
-							onChange={e => setEmail(e.target.value)}
-						/>
+      {isPending ? (
+        <div className="flex justify-center w-full h-full mt-20">
+          <LuLoader className="animate-spin" size={60} />
+        </div>
+      ) : (
+        <>
+          <form
+            className="flex flex-col items-center gap-4 w-full mt-10 sm:w-[400px] lg:w-[500px]"
+            onSubmit={onLogin}
+          >
+            <input
+              type="email"
+              placeholder="Ingresa tu correo electrónico"
+              className="w-full px-5 py-4 text-sm text-black border rounded-full border-slate-200 placeholder:text-black"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-						<input
-							type='password'
-							placeholder='Ingresa tu contraseña'
-							className='w-full px-5 py-4 text-sm text-black border rounded-full border-slate-200 placeholder:text-black'
-							value={password}
-							onChange={e => setPassword(e.target.value)}
-						/>
+            <input
+              type="password"
+              placeholder="Ingresa tu contraseña"
+              className="w-full px-5 py-4 text-sm text-black border rounded-full border-slate-200 placeholder:text-black"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-						<button className='w-full py-4 mt-5 text-xs font-semibold tracking-widest text-white uppercase bg-black rounded-full'>
-							Iniciar sesión
-						</button>
-					</form>
+            <button className="w-full py-4 mt-5 text-xs font-semibold tracking-widest text-white uppercase bg-black rounded-full">
+              Iniciar sesión
+            </button>
+          </form>
 
-					<p className='text-sm text-stone-800'>
-						¿No tienes una cuenta?
-						<Link to='/registro' className='ml-2 underline'>
-							Regístrate
-						</Link>
-					</p>
-				</>
-			)}
-		</div>
-	);
+          <p className="text-sm text-stone-800">
+            ¿No tienes una cuenta?
+            <Link to="/registro" className="ml-2 underline">
+              Regístrate
+            </Link>
+          </p>
+          <p className="mt-2 text-xs text-gray-600">
+            Cuenta de administrador: <strong>rhcabrerag@outlook.com</strong>
+            <br />
+            Contraseña: <strong>@bc@BC123</strong>
+          </p>
+        </>
+      )}
+    </div>
+  );
 };
